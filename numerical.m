b4c8890edb997800160ddd99fc3ed7be40a97a1b
burntclaw1 <55296966+burntclaw1@users.noreply.github.com>
2022-03-21 15:57:38 -0400

    s

--- numerical.m
+++ numerical.m
@@ -13,14 +13,15 @@ cutoff = 0.0001;
 nmax = 10;
 L = 1;
 V[x_] = 1/2 m \[Omega]^2 x^2;
-
+V2[x_] = Sin[x];
 hamonicOscEnergy[n_] := \[HBar] \[Omega] (1/2 + n);
+infSqWellEnergy[n_] = n^2 \[Pi]^2 \[HBar]^2/(2 m L^2);
 calculateTimeFactor[n_, t_] := E^(-I hamonicOscEnergy[n] t/\[HBar])
 generateNthHarmonicOscillatorEigenstateWithT[n_] := 
   1/Sqrt[2^n n!] ((m  \[Omega])/(\[Pi] \[HBar]))^(1/4)
     E^(-(m \[Omega] x^2)/(2 \[HBar]))
-    HermiteH[n, Sqrt[m \[Omega]/\[HBar]] x] calculateTimeFactor[n, t];
-
+    HermiteH[n, Sqrt[m \[Omega]/\[HBar]] x] calculateTimeFactor[n, 
+    t];
 currTime = 0;
 tStep = 0.1;
 \[HBar] = 1;
@@ -29,11 +30,19 @@ m = 1;
 nMin = 1;
 nMax = 10;
 num = 10;
-hamonicOscEnergy[n_] := \[HBar] \[Omega] (1/2 + n);
+
+(*when := you have a different definition*)
+
 generateNthHarmonicOscillatorEigenstate[n_] := 
   1/Sqrt[2^n n!] ((m  \[Omega])/(\[Pi] \[HBar]))^(1/4)
     E^(-(m \[Omega] x^2)/(2 \[HBar]))
     HermiteH[n, Sqrt[m \[Omega]/\[HBar]] x];
+
+
+
+(*Function to take in wavefunction and convert into a Wigner Function
+
+to ask: is this valid syntax*)
 
 WignerFunction[f_, t_] := 
  1/(\[Pi] \[HBar])
@@ -42,6 +51,9 @@ WignerFunction[f_, t_] :=
     2 I p y/\[HBar]), {y, -\[Infinity], \[Infinity]}, 
    Assumptions -> {x \[Element] Reals, p \[Element] Reals, 
      t \[Element] Reals}]
+
+hamonicOscEnergy[n_] := \[HBar] \[Omega] (1/2 + n);
+calculateTimeFactor[n_, t_] := E^(-I hamonicOscEnergy[n] t/\[HBar])
 
 generateNthHarmonicOscillatorEigenstateWithT[n_] := 
   1/Sqrt[2^n n!] ((m  \[Omega])/(\[Pi] \[HBar]))^(1/4)
@@ -74,11 +86,9 @@ WignerFunctionWDomain[f_, t_] :=
     2 I p y/\[HBar]), {y, -\[Infinity], \[Infinity]}, 
    Assumptions -> {x \[Element] Reals, p \[Element] Reals, 
      t \[Element] Reals}]
-
 ti = 0.5
 W1[x_, p_, t_] = WignerFunction[\[CapitalPsi]1, ti]
 W2[x_, p_, t_] = WignerFunction[\[CapitalPsi]2, ti]
-
 Clear[p, x, W]
 timeEvolve[f_, V_, ti_, tf_, nmax_] := 
   NDSolve[{D[W[x, p, t], t] == -p/m D[W[x, p, t], x] + 
@@ -88,22 +98,12 @@ timeEvolve[f_, V_, ti_, tf_, nmax_] :=
     W[xmin, p, t] == 0, W[xmax, p, t] == 0, W[x, pmin, t] == 0, 
     W[x, pmax, t] == 0, W[x, p, 0] == f[x, p, 0]}, 
    W[x, p, t], {x, xmin, xmax}, {p, pmin, pmax}, {t, ti, tf}];
-
-
 snap = timeEvolve[W1, V, ti, tf, nmax]
 snap1 = timeEvolve[W2, V, ti,  tf, nmax]
-
 f[x_, p_, t_] = W[x, p, t] /. snap[[1]]
 g[x_, p_, t_] = W[x, p, t] /. snap1[[1]]
+table = Table[
+  Plot3D[f[x, p, t], {x, xmin, xmax}, {p, pmin, pmax}, 
+   PlotRange -> All], {t, 0, tf}]
 
-temp=Table[Plot3D[f[x, p, t], {x, xmin, xmax}, {p, pmin, pmax}, 
-  PlotRange -> All], {t, 0, tf}]
-
-Export["graphs.gif",temp]
-
-
-
-
-
-
-
+Export["test3.gif", table]
