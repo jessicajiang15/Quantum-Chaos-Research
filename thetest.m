a5f4e40c58ed0b06ffaf4f50491ea98c63ad137e
burntclaw1 <55296966+burntclaw1@users.noreply.github.com>
2022-03-27 17:33:06 -0400

    s

--- thetest.m
+++ thetest.m
@@ -7,7 +7,7 @@
 (* ::Input::Initialization:: *)
 currTime=0;
 totalSteps=100;
-tf=30;
+tf=10.1;
 ti=0.1;
 \[HBar]=1;
 \[Omega]=0.5;
@@ -98,16 +98,62 @@ g[x_,p_,t_]=W[x,p,t]/.snap1[[1]]
 
 
 (* ::Input::Initialization:: *)
-Plot3D[f[x,p,0],{x,xmin,xmax},{p,pmin,pmax},PlotRange->All]
+calculateEMD[finitnorm_, ffinalnorm_, xmin_, xmax_,ymin_, ymax_, nbox_, gridboxcutoff_]:=Module[{dx, dy, diffarray, outboxes, inboxes, nout, nin, nvars, mat0, supplyamount, demandamount, mat, distance, k, l, m, n, i, j},dx=(xmax-xmin)/nbox;dy=(ymax-ymin)/nbox;
+f1[x_,p_]=finitnorm;
+f2[x_,p_]=ffinalnorm;
+Print["hello"];
+Monitor[diffarray=Table[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,xmin+(i-1)*dx,xmin+i*dx},{p,pmin+(j-1)*dy,pmin+j*dy},Method->{"AdaptiveMonteCarlo",MaxPoints->10^7}],{i,1,nbox},{j,1,nbox}],i];
+Print["done"];
+outboxes={};inboxes={}; 
+For[i=1,i<=nbox,i++,
+For[j=1,j<=nbox,j++,
+If[diffarray[[i,j]]>gridboxcutoff,AppendTo[outboxes,{xmin+(i-.5)*dx,ymin+(j-.5)*dy}]];
+If[diffarray[[i,j]]<-gridboxcutoff,AppendTo[inboxes,{xmin+(i-.5)*dx,ymin+(j-.5)*dy}]]]];
+nout=Length[outboxes];nin=Length[inboxes];
+nvars=nout*nin;
+supplyamount ={}; demandamount = {};
+For[i=1,i<=nbox,i++,
+For[j=1,j<=nbox,j++,
+If[diffarray[[i,j]]>gridboxcutoff,AppendTo[supplyamount,diffarray[[i,j]]]];
+If[diffarray[[i,j]]<-gridboxcutoff,AppendTo[demandamount,diffarray[[i,j]]]];
+]];
+mat = Table[
+EuclideanDistance[outboxes[[o]], inboxes[[q]]],{o,Length@outboxes},{q, Length@inboxes}];
+mat0 = Table[0,{Length@outboxes+Length@inboxes},{Length@outboxes+Length@inboxes}];
+For[i=1,i<=Length@outboxes,i++,
+For[j=Length@outboxes+1,j<=(Length@inboxes+Length@outboxes),j++,
+mat0[[i,j]]=mat[[i,j-Length@outboxes]]
+]];
+
+mat0//MatrixForm;
+
+(*Then put in the numbers for the upper left section*)
+For[k=1,k<=Length@outboxes,k++,
+For[l=k+1,l<=Length@outboxes,l++,
+mat0[[k,l]]=EuclideanDistance[outboxes[[k]], outboxes[[l]]]
+]];
+
+mat0//MatrixForm;
+(*For the lower right..*)
+For[m=Length@outboxes+1,m<=(Length@inboxes+Length@outboxes-1),m++,
+For[n=m+1,n<=(Length@inboxes+Length@outboxes),n++,
+mat0[[m,n]]=EuclideanDistance[inboxes[[m-Length@outboxes]], inboxes[[n-Length@outboxes]]]
+]];
+
+mat0//MatrixForm;
+distance=FindMinimumCostFlow[ mat0, Join[supplyamount,demandamount]];
+Print["Done one"];
+Return[distance];
+]
+
+
 
 
 (* ::Input::Initialization:: *)
+nbox=20;
+gridboxcutoff=.0001;
+distances=Table[calculateEMD[f[x,p,t], g[x, p, t], xmin, xmax,pmin, pmax, nbox, gridboxcutoff],{t,ti, tf,1}]
+plt=ListPlot[distances]
+Export["plot.gif",plt]
 
 
-
-(* ::Input::Initialization:: *)
-temp=Table[Plot3D[f[x,p,t],{x,xmin,xmax},{p,pmin,pmax},PlotRange->All],{t,0,tf}]
-
-
-(* ::Input::Initialization:: *)
-Export["thetestt2.gif",temp]
