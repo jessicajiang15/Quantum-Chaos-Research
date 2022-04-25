946a29f64b2a153202e83fb7cde6776a931d5633
burntclaw1 <55296966+burntclaw1@users.noreply.github.com>
2022-04-25 15:30:59 -0400

    S

--- directTimeEvo.m
+++ directTimeEvo.m
@@ -2,9 +2,7 @@
 
 (* ::Input::Initialization:: *)
  
-Unprotect[$ProcessorCount];$ProcessorCount = 8;
- 
-Print[ParallelEvaluate[$MachineName]];
+
 
 (* ::Input::Initialization:: *)
 currTime=0;
@@ -25,10 +23,12 @@ L=1;
 
 (* ::Input::Initialization:: *)
 V[x_]=1/2 m \[Omega]^2 x^2;
+V2[x_]=Sin[x];
 
 
 (* ::Input::Initialization:: *)
 hamonicOscEnergy[n_]:=\[HBar] \[Omega](1/2+n);
+infSqWellEnergy[n_]=n^2 \[Pi]^2 \[HBar]^2/(2m L^2);
 calculateTimeFactor[n_, t_]:=E^(-I hamonicOscEnergy[n] t/\[HBar])
 generateNthHarmonicOscillatorEigenstateWithT[n_]:=1/Sqrt[2^n n!] ((m  \[Omega])/(\[Pi] \[HBar]))^(1/4) E^(-(m \[Omega] x^2)/(2 \[HBar])) HermiteH[n, Sqrt[m \[Omega]/\[HBar]]x]calculateTimeFactor[n, t];
 
@@ -85,8 +85,16 @@ W2[x_, p_,t_]=WignerFunction[\[CapitalPsi]2, ti]
 
 
 (* ::Input::Initialization:: *)
+
+
+
+(* ::Input::Initialization:: *)
 Clear[p,x,W]
 timeEvolve[f_,V_, ti_,tf_, nmax_]:=NDSolve[{D[W[x, p, t],t]==-p/m D[W[x, p, t],x]+Sum[(1/2)^(2n) 1/(2n+1)! (D[V[x],{x,2n+1} ])(D[W[x,p,t],{p,2n+1}]),{n,0,nmax}], W[xmin,p,t]==0,W[xmax,p,t]==0, W[x,pmin,t]==0,W[x,pmax,t]==0,W[x,p,0]==f[x,p, 0]},W[x,p,t],{x,xmin,xmax},{p,pmin,pmax},{t,ti, tf}];
+
+
+(* ::Input::Initialization:: *)
+p
 
 
 (* ::Input::Initialization:: *)
@@ -97,6 +105,18 @@ snap1=timeEvolve[W2, V,ti,  tf, nmax]
 (* ::Input::Initialization:: *)
 f[x_,p_,t_]=W[x,p,t]/.snap[[1]]
 g[x_,p_,t_]=W[x,p,t]/.snap1[[1]]
+
+
+(* ::Input::Initialization:: *)
+Plot3D[f[x,p,0],{x,xmin,xmax},{p,pmin,pmax},PlotRange->All]
+
+
+(* ::Input::Initialization:: *)
+
+
+
+(* ::Input::Initialization:: *)
+Table[Plot3D[f[x,p,t],{x,xmin,xmax},{p,pmin,pmax},PlotRange->All],{t,0,tf}]
 
 
 (* ::Input::Initialization:: *)
@@ -152,11 +172,101 @@ Return[distance];
 
 
 (* ::Input::Initialization:: *)
+
+nbox=20;
+gridboxcutoff=.0001;
+Table[calculateEMD[f[x,p,t], g[x, p, t], xmin, xmax,pmin, pmax, nbox, gridboxcutoff],{t,ti, tf}]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[f[x,p,0.7],{x,xmin,xmax},{p,pmin,pmax}, Method->"AdaptiveMonteCarlo"]]
+lol[x_,p_,t_]=WignerFunction[\[CapitalPsi]1, 0.7]
+Timing[NIntegrate[lol[x,p,0.7],{x,xmin,xmax},{p,pmin,pmax}]]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-1},{p,0,-1}]]
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-4.5},{p,0,-4.5}]]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-4.5},{p,0,-4.5}]]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-4.5},{p,0,-4.5},PrecisionGoal->3,Method->{"AdaptiveMonteCarlo",MaxPoints->10^7,"SymbolicProcessing"->0}]]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-4.5},{p,0,-4.5},Method->{"AdaptiveQuasiMonteCarlo"}]]
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,0,-4.5},{p,0,-4.5},PrecisionGoal->3,Method->{"InterpolationPointsSubdivision","SymbolicProcessing"->0}]]
+
+
+(* ::Input::Initialization:: *)
+diffarray={};
+For[i=1,i<=3, For[j=1,j<=3,
+If[
+Re[NIntegrate[Re[f1[x,p]-f2[x,p]],{x,xmin+(i-1)*dx,xmin+i*dx},{p,ymin+(j-1)*dy,ymin+j*dy},Method->{"MonteCarlo","MaxPoints"->10}]>10^-7],
+Append[diffarray,NIntegrate[
+Re[f1[x,p]-f2[x,p]],{x,xmin+(i-1)*dx,xmin+i*dx},{p,ymin+(j-1)*dy,ymin+j*dy},Method->{"AdaptiveMonteCarlo","MaxPoints"->10^7}]
+];
+Print["done one integral"];
+]]];
+
+
+(* ::Input::Initialization:: *)
+nbox=20
+dx=(xmax-xmin)/nbox;dy=(pmax-pmin)/nbox;
+diffarray={};
+Timing[For[i=1,i<=nbox, For[j=1,j<=nbox,
+Append[diffarray,NIntegrate[
+Re[f[x,p,0.1]-g[x,p,0.1]],{x,xmin+(i-1)*dx,xmin+i*dx},{p,pmin+(j-1)*dy,pmin+j*dy},PrecisionGoal->4,Method->"InterpolationPointsSubdivision"]
+];
+]]];
+
+
+(* ::Input::Initialization:: *)
+nbox=10
+dx=(xmax-xmin)/nbox;dy=(pmax-pmin)/nbox;
+diffarray={};
+
+
+
+
+(* ::Input::Initialization:: *)
+
+Timing[Monitor[table=Table[Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,xmin+(i-1)*dx,xmin+i*dx},{p,pmin+(j-1)*dy,pmin+j*dy},Method->{"AdaptiveMonteCarlo",MaxPoints->10^7}]],{i,1,nbox},{j,1,nbox}],i]]
+
+
+(* ::Input::Initialization:: *)
+xmin+(1-1)*dx
+xmin+1*dx
+pmin+(1-1)*dy
+pmin+1*dy
+
+
+(* ::Input::Initialization:: *)
+Timing[NIntegrate[Re[f[x,p,0.1]-g[x,p,0.1]],{x,xmin+(1-1)*dx,xmin+1*dx},{p,pmin+(1-1)*dy,pmin+1*dy},Method->{"AdaptiveMonteCarlo",MaxPoints->10^7}]]
+
+
+(* ::Input::Initialization:: *)
+table
+
+
+(* ::Input::Initialization:: *)
+
+
+
+(* ::Input::Initialization:: *)
 nbox=20;
 gridboxcutoff=.0001;
 distances=Table[calculateEMD[f[x,p,t], g[x, p, t], xmin, xmax,pmin, pmax, nbox, gridboxcutoff],{t,ti, tf,1}]
 plt=ListPlot[distances]
 Export["plotnbox20reRun.gif",plt]
 Export["numbersnbox20monteRerun.csv",distances,"CSV"]
+
 
 
